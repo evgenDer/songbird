2d27ded7c98b0a7aa76c72b685b52a3a12846a3b
Evgeniya <evgeniya375447249020@gmail.com>
2020-08-13 22:23:54 +0300

    feat: add bird details component

---
+++ src/components/bird-details/bird-details.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+import "./bird-details.css";
+import AudioPlayer from "react-h5-audio-player";
+
+export default class BirdDetails extends Component {
+  state = {
+    bird: null,
+  };
+
+  componentDidMount() {
+    this.updateBird();
+  }
+
+  componentDidUpdate(prevProps) {
+    console.log();
+    if (this.props.birdId !== prevProps.birdId) {
+      this.updateBird();
+    }
+  }
+
+  updateBird() {
+    const { bird } = this.props;
+    console.log(bird);
+    if (!bird) {
+      return;
+    }
+
+    this.setState({ bird });
+  }
+
+  render() {
+    const { bird } = this.props;
+
+    if (!bird) {
+      return <span>Послушайте плеер. Выберите птицу из списка</span>;
+    }
+
+    const { name, audio, species, description, image } = bird;
+
+    return (
+      <div className="bird-details">
+        <div className="bird-details__body">
+          <img src={image} alt={name} />
+          <div className="bird-info">
+            <h3>{name}</h3>
+            <hr></hr>
+            <h4>{species}</h4>
+            <hr></hr>
+            <AudioPlayer
+              className="question-block__audio_player"
+              src={audio}
+              autoPlayAfterSrcChange={false}
+              ref={(current) => (this.player = current)}
+            />
+          </div>
+        </div>
+        <div className="bird-details__description">
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
